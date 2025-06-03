7361eb6058d9a0292431b46f69bd9973f03cabb2
Andres Raba <andresm-rabab@javeriana.edu.co>
2025-06-03 12:20:34 -0500

    Se agregó nombre a label de nombre de receptor

--- Proyecto/src/main/java/com/fundamentos/proyecto/controller/ContactoController.java
+++ Proyecto/src/main/java/com/fundamentos/proyecto/controller/ContactoController.java
@@ -1,12 +1,20 @@
 package com.fundamentos.proyecto.controller;
 
+import com.fundamentos.proyecto.model.Usuario;
 import com.fundamentos.proyecto.services.MensajeService;
+import com.fundamentos.proyecto.services.UsuarioService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
 public class ContactoController {
+
+    private Usuario receptor;
+
+    @FXML
+    private Label labelNombreReceptor;
 
     @FXML
     private TextArea mensajeTextArea;
@@ -25,8 +33,17 @@ public class ContactoController {
 
     public void setReceptorId(int receptorId) {
         this.receptorId = receptorId;
+        cargarDatosReceptor();
     }
 
+    private void cargarDatosReceptor() {
+        receptor = new UsuarioService().obtenerUsuarioPorId(receptorId);
+        if (receptor != null) {
+            labelNombreReceptor.setText("Enviar mensaje a: " + receptor.getNombre() + " " + receptor.getApellido());
+        } else {
+            labelNombreReceptor.setText("Usuario no encontrado");
+        }
+    }
     private void enviarMensaje() {
         String contenido = mensajeTextArea.getText();
 
