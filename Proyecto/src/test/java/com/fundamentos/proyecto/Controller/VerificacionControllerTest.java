e59fae29e203b3485313bf002dd95f7cf385c269
sebastiansanchez <tiansv@gmail.com>
2025-06-04 18:18:13 -0500

    testVerificacionExitosaSanti

--- Proyecto/src/test/java/com/fundamentos/proyecto/Controller/VerificacionControllerTest.java
+++ Proyecto/src/test/java/com/fundamentos/proyecto/Controller/VerificacionControllerTest.java
@@ -26,5 +26,18 @@ public class VerificacionControllerTest {
         assertTrue(resultado, "Juan debería pasar la verificación con sus datos correctos");
     }
 
+    @Test
+    void testVerificacionExitosaSanti() {
+        String correo = "santi@gmail.com";
+        String pregunta = "nombre del barrio en donde creció";
+        String respuesta = "bogota";
+
+        boolean resultado = usuarioService.validarRecuperacion(correo, pregunta, respuesta);
+
+        assertTrue(resultado, "Santi debería pasar la verificación con sus datos correctos");
+    }
+
+
+
 
 }
